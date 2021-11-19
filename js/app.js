14223486ef3f3141e9061188da4118f3b97b5830
Darryn Codes <hello@darryncodes.co.uk>
2021-11-19 17:30:41 +0000

    planning - starting to plan javascript logic to inform html structure, class and id usage

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// plan for JS logic
+
+// select all elments by id
+// or should I be looping over all the inputs?
+// do i need to prevent form from submitting: e.preventDefault();
+// update fields when info gets entered
+// prevent negative numbers?
+// reset all fields
+// add active states
+// add erorr number of people cant be zero
+
+// bill
+// - input field
+// - store value in vairable and make sure its a number
+// parseInt(bill.value); ??
+
+// tip
+// - input field
+// - store value in variable
+// - tip.value ??
+
+// tip amount
+// bill / 100 * tip
+
+// number of people
+// - input field
+// - store value in variable
+
+// tip amount per person
+// tip amount / number of people
+
+// total bill per person
+// bill per person = bill / number of people
+// tip amount + bill per person
